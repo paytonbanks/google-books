bb5b4a67be0d65688b8a79e2b2d7eb5cfb70d072
Payton Banks <paytonbanks@Mac-Payton.local>
2020-10-27 21:51:34 -0400

    File structure: controllers, models, routes. Server: required routes. removed notation.

--- server.js
+++ server.js
@@ -2,19 +2,16 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require('./routes');
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Send every other request to the React app
-// Define any API routes before this runs
+app.use(routes);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
