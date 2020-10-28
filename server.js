8f9bf9adabe30c109adbc65f7d529ca2759409e5
Payton Banks <paytonbanks@Mac-Payton.local>
2020-10-27 22:21:28 -0400

    Connected external depnendecy to enviro-Mongo, confirmed .env to gitignore, required mongoose in server, URL parser true

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -15,6 +16,8 @@ app.use(routes);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gbooks", {useNewUrlParser: true});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
